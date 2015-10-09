3dc14f79a537ab1306cc1ab8600fb0395aa49468
Allan T. Harper <ath@caselle.com>
2015-10-09 13:21:53 -0600

    Initial commit

---
+++ File Watcher/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace File_Watcher
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.fileTextBox = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.runTextBox = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.logTextBox = new System.Windows.Forms.TextBox();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(74, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Watch this file";
+      // 
+      // fileTextBox
+      // 
+      this.fileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.fileTextBox.Location = new System.Drawing.Point(12, 25);
+      this.fileTextBox.Name = "fileTextBox";
+      this.fileTextBox.Size = new System.Drawing.Size(541, 20);
+      this.fileTextBox.TabIndex = 1;
+      this.fileTextBox.Text = "Z:\\CopyBot2\\Development_roadsign.txt";
+      this.fileTextBox.TextChanged += new System.EventHandler(this.fileTextBox_TextChanged);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(12, 48);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(127, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Run this when it changes";
+      // 
+      // runTextBox
+      // 
+      this.runTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.runTextBox.Location = new System.Drawing.Point(12, 64);
+      this.runTextBox.Name = "runTextBox";
+      this.runTextBox.Size = new System.Drawing.Size(541, 20);
+      this.runTextBox.TabIndex = 3;
+      this.runTextBox.Text = "StartMove.bat";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(12, 99);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(25, 13);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "Log";
+      // 
+      // logTextBox
+      // 
+      this.logTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.logTextBox.Location = new System.Drawing.Point(12, 115);
+      this.logTextBox.Multiline = true;
+      this.logTextBox.Name = "logTextBox";
+      this.logTextBox.Size = new System.Drawing.Size(541, 184);
+      this.logTextBox.TabIndex = 5;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(565, 311);
+      this.Controls.Add(this.logTextBox);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.runTextBox);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.fileTextBox);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox fileTextBox;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox runTextBox;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox logTextBox;
+  }
+}
+
---
+++ File Watcher/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Watcher
+{
+  public partial class Form1 : Form
+  {
+    private FileSystemWatcher _watcher;
+    private string _lastRanVersion;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      _watcher = new FileSystemWatcher
+      {
+        Path = Path.GetDirectoryName(fileTextBox.Text),
+        Filter = Path.GetFileName(fileTextBox.Text),
+        NotifyFilter = NotifyFilters.LastWrite,
+        EnableRaisingEvents = true
+      };
+
+      _watcher.Changed += Changed;
+    }
+
+    private bool _running;
+    private void Changed(object sender, FileSystemEventArgs e)
+    {
+      if (_running) return;
+      var version = VersionNumber();
+      if (_lastRanVersion == version) return;
+      _lastRanVersion = version;
+
+      _running = true;
+      logTextBox.Invoke(new Action(() => logTextBox.AppendText(LogText())));
+      if (runTextBox.Text.Length > 0 && File.Exists(runTextBox.Text))
+      {
+        var p = Process.Start(runTextBox.Text);
+        p.WaitForExit();
+      }
+
+      _running = false;
+    }
+
+    private string LogText()
+    {
+      return string.Format("File Changed at {0} Version Number: {2} {1}", DateTime.Now, Environment.NewLine,_lastRanVersion);
+    }
+
+    private static string VersionNumber()
+    {
+      const string copybotDir = @"\\CASELLE3\Apps3\CopyBot2";
+      var newestDirname = File.ReadAllLines(Path.Combine(copybotDir, "Development_roadsign.txt"))[0];
+      var newestDir = Path.Combine(copybotDir, newestDirname);
+      var fileName = Path.Combine(newestDir, "Caselle.exe");
+      if (!File.Exists(fileName)) return string.Empty;
+      return FileVersionInfo.GetVersionInfo(fileName).ProductVersion;
+    }
+
+    private void fileTextBox_TextChanged(object sender, EventArgs e)
+    {
+      _watcher.Path = Path.GetDirectoryName(fileTextBox.Text);
+      _watcher.Filter = Path.GetFileName(fileTextBox.Text);
+    }
+  }
+}
